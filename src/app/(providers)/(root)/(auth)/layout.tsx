d60bb02d7fd891e1538f048facf45d63323bcae6
hayanLee <hayanlee.dev@gmail.com>
2024-07-10 01:05:22 +0900

    Design : AuthPage 포켓볼에 animation 추가

--- src/app/(providers)/(root)/(auth)/layout.tsx
+++ src/app/(providers)/(root)/(auth)/layout.tsx
@@ -7,11 +7,11 @@ import { PropsWithChildren } from 'react';
 function AuthLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex h-full flex-col justify-center bg-blue-200 p-10">
-      <div className="flex items-center justify-center">
-        <Image src={ballOne} alt="" width={40} height={40} />
-        <Image src={ballTwo} alt="" width={40} height={40} />
-        <Image src={ballThree} alt="" width={40} height={40} />
-        <Image src={ballFour} alt="" width={40} height={40} />
+      <div className="flex animate-bounce items-center justify-center">
+        <Image src={ballOne} alt="" width={40} height={40} className="bounce-delay-1" />
+        <Image src={ballTwo} alt="" width={40} height={40} className="bounce-delay-2" />
+        <Image src={ballThree} alt="" width={40} height={40} className="bounce-delay-3" />
+        <Image src={ballFour} alt="" width={40} height={40} className="bounce-delay-4" />
       </div>
 
       <h1 className="text-center text-3xl font-semibold">PokeGotchi</h1>
