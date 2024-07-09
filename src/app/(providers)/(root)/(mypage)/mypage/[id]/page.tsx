8d10c45c1d979f125d01343673954dd73f8b63ab
hwangseunghyun <hwamg123@naver.com>
2024-07-09 19:49:59 +0900

    Design: ui 수정

--- src/app/(providers)/(root)/(mypage)/mypage/[id]/page.tsx
+++ src/app/(providers)/(root)/(mypage)/mypage/[id]/page.tsx
@@ -20,24 +20,28 @@ const Page: React.FC = () => {
   }
 
   return (
-    <div className="flex items-center justify-center bg-gray-100 overflow-hidden">
+    <div className="flex items-center justify-center overflow-hidden bg-gray-100">
       <div className="w-[600px] bg-white p-7">
         <div className="mb-8">
-          <div className="flex items-center justify-between rounded-lg border border-gray-300 bg-white-200 p-4">
-            <div className="flex items-center">
-              <div className="w-24 h-24 relative">
+          <div className="bg-white-200 relative flex items-center justify-between shadow-sm rounded-lg border border-gray-300 p-4">
+            <div className="relative flex items-center">
+              <div className="relative h-24 w-24">
                 <Image src={user.profileImage} alt="userImage" layout="fill" className="rounded-full object-cover" />
               </div>
-              <div className="ml-5 flex flex-col">
-                <div className="text-lg font-bold mb-2">{user.name}</div>
-                {user.hashtags.map((hashtag) => (
-                  <div key={hashtag.id}>
-                    <h4 className="font-lighter">#{hashtag.hashtag}</h4>
-                  </div>
-                ))}
+              <div className="ml-5 relative">
+                <div className="absolute top-0 left-0 text-lg font-bold">{user.name}</div>
+                <div className="mt-6">
+                  {user.hashtags.map((hashtag) => (
+                    <div key={hashtag.id}>
+                      <h4 className="text-xs font-light">#{hashtag.hashtag}</h4>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-            <button className="rounded-md border border-gray-300 bg-gray-100 px-2 py-1 text-xs">수정</button>
+            <button className="absolute bottom-4 right-4 rounded-md border border-gray-300 bg-gray-100 px-2 py-1 text-xs">
+              수정
+            </button>
           </div>
         </div>
         <h2 className="mb-4 text-2xl font-bold">내 포켓몬</h2>
@@ -45,13 +49,19 @@ const Page: React.FC = () => {
           {user.pokemons.map((mypokemon) => (
             <div
               key={mypokemon.id}
-              className="rounded-lg bg-white-100 p-4 shadow-sm transform transition duration-300 hover:shadow-lg hover:scale-105 border border-gray-300"
+              className="bg-white-100 transform rounded-lg border border-gray-300 p-4 shadow-sm transition duration-300 hover:scale-105 hover:shadow-lg"
             >
               <div className="flex flex-col items-center">
-                <div className="relative mb-4 w-24 h-24">
-                  <Image src={mypokemon.image} alt={mypokemon.name} fill className="rounded-full object-cover" sizes="100%" />
+                <div className="relative mb-4 h-24 w-24">
+                  <Image
+                    src={mypokemon.image}
+                    alt={mypokemon.name}
+                    fill
+                    className="rounded-full object-cover"
+                    sizes="100%"
+                  />
                 </div>
-                <h3 className="mb-2 font-bold text-sm">{mypokemon.name}</h3>
+                <h3 className="mb-2 text-sm font-bold">{mypokemon.name}</h3>
                 <button className="rounded-md border border-gray-300 bg-gray-100 px-2 py-1 text-xs">상세정보</button>
               </div>
             </div>
