77951ea171545f050d2e3bf7b12c69cb7ee6b36d
hwijinkim22 <bout9396@naver.com>
2024-07-09 14:20:14 +0900

    Rename: 포켓몬 api 폴더 변경

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+
+import axios from 'axios';
 
 export default function Home() {
+  const fetchPokemonData = async () => {
+    try {
+      const response = await axios.get("/api");
+      const data = response.data;
+    } catch (error) {
+      console.error("Response Error:", error);
+    }
+
+  }
+
+
   return (
     <>
       ㅇㅇㅇ
