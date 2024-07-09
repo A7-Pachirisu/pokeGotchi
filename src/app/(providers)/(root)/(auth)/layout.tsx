a6e7f17a5d3483c7be4442dce1d3548819c51900
hayanLee <hayanlee.dev@gmail.com>
2024-07-10 02:03:01 +0900

    Comment : 잘못된 className 삭제

--- src/app/(providers)/(root)/(auth)/layout.tsx
+++ src/app/(providers)/(root)/(auth)/layout.tsx
@@ -8,10 +8,10 @@ function AuthLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex h-full flex-col justify-center bg-blue-200 p-10">
       <div className="flex animate-bounce items-center justify-center">
-        <Image src={ballOne} alt="" width={40} height={40} className="bounce-delay-1" />
-        <Image src={ballTwo} alt="" width={40} height={40} className="bounce-delay-2" />
-        <Image src={ballThree} alt="" width={40} height={40} className="bounce-delay-3" />
-        <Image src={ballFour} alt="" width={40} height={40} className="bounce-delay-4" />
+        <Image src={ballOne} alt="" width={40} height={40} />
+        <Image src={ballTwo} alt="" width={40} height={40} />
+        <Image src={ballThree} alt="" width={40} height={40} />
+        <Image src={ballFour} alt="" width={40} height={40} />
       </div>
 
       <h1 className="text-center text-3xl font-semibold">PokeGotchi</h1>
