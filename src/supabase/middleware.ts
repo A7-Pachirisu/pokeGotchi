edd469e2f6557fe3c3a3a9d15d949ca8182b1afb
hayanLee <hayanlee.dev@gmail.com>
2024-07-10 02:00:33 +0900

    Comment : 미들웨어 경로 로그인 구현 전까지 주석 처리

--- src/supabase/middleware.ts
+++ src/supabase/middleware.ts
@@ -33,17 +33,19 @@ export async function updateSession(request: NextRequest) {
     data: { user }
   } = await supabase.auth.getUser();
 
-  if (
-    !user &&
-    request.nextUrl.pathname !== '/' &&
-    !request.nextUrl.pathname.startsWith('/log-in') &&
-    !request.nextUrl.pathname.startsWith('/sign-up')
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
-    const url = request.nextUrl.clone();
-    url.pathname = '/login';
-    return NextResponse.redirect(url);
-  }
+  //로그인 적용 전까지 임시로 주석처리
+  // if (
+  //   !user &&
+  //   request.nextUrl.pathname !== '/' &&
+  //   request.nextUrl.pathname.startsWith('game') &&
+  //   !request.nextUrl.pathname.startsWith('/log-in') &&
+  //   !request.nextUrl.pathname.startsWith('/sign-up')
+  // ) {
+  //   // no user, potentially respond by redirecting the user to the login page
+  //   const url = request.nextUrl.clone();
+  //   url.pathname = '/log-in';
+  //   return NextResponse.redirect(url);
+  // }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
