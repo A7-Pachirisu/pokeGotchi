63b7bda7332ca3a66be13bd0cede5ff4e6017043
hayanLee <hayanlee.dev@gmail.com>
2024-07-09 13:45:03 +0900

    Chore : metaData 수정 및 QueryProvider 재연결
    - 안 쓰는 파일, 내용 정리

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import QueryProvider from './_providers/QueryProvider';
 
 function ProvidersLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return <QueryProvider>{children}</QueryProvider>;
 }
 
 export default ProvidersLayout;
