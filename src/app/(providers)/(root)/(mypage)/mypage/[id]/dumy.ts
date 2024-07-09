8a82dd07419b4110083ee52cbb3067b571665851
hwangseunghyun <hwamg123@naver.com>
2024-07-09 16:08:34 +0900

    Rename : [id]폴더 생성 후 mypage이동 , Refactor: 사용자 더미데이터로 변경 후 userid 별 라우터 추가

---
+++ src/app/(providers)/(root)/(mypage)/mypage/[id]/dumy.ts
@@ -0,0 +1,139 @@
+// src/data/dummy.ts
+import '../../../../../../assets/pokemon1.png'
+export interface Pokemon {
+  id: number;
+  name: string;
+  image: string;
+  abilities: string[];
+}
+
+export interface Hashtags {
+  id: number;
+  hashtag: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  profileImage: string;
+  hashtags: Hashtags[];
+  pokemons: Pokemon[];
+}
+
+const users: User[] = [
+  {
+    id: 1,
+    name: 'user1',
+    profileImage: '/Pokeball.png',
+    hashtags: [{ id: 1, hashtag: '갸라도스의 폭풍속에서' }],
+    pokemons: [
+      { id: 1, name: '이상해씨', image: '/img1.png', abilities: ['심록', '엽록소'] },
+      { id: 2, name: '파이리', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 3, name: '꼬부기', image: '/img1.png', abilities: ['급류', '빗의접시'] },
+      { id: 4, name: '피카츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] }
+    ]
+  },
+  {
+    id: 2,
+    name: 'user2',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 2, hashtag: '내일은 세계최고의 포켓몬트레이너' }],
+    pokemons: [
+      { id: 5, name: '이브이', image: '/img1.png', abilities: ['적응력', '도주'] },
+      { id: 6, name: '뮤', image: '/img1.png', abilities: ['싱크로'] },
+      { id: 7, name: '뮤츠', image: '/img1.png', abilities: ['프레셔', '정신력'] }
+    ]
+  },
+  {
+    id: 3,
+    name: 'user3',
+    profileImage: '/bg1.png',
+    hashtags: [{ id: 3, hashtag: '포켓몬 마스터를 향해' }],
+    pokemons: [
+      { id: 8, name: '라이츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 9, name: '리자몽', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 10, name: '프리저', image: '/img1.png', abilities: ['눈보라', '빙판'] },
+      { id: 1, name: '이상해씨', image: '/img1.png', abilities: ['심록', '엽록소'] }
+    ]
+  },
+  {
+    id: 4,
+    name: 'user4',
+    profileImage: '/default_ball.png',
+    hashtags: [{ id: 4, hashtag: '전설의 포켓몬을 찾아서' }],
+    pokemons: [
+      { id: 2, name: '파이리', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 3, name: '꼬부기', image: '/img1.png', abilities: ['급류', '빗의접시'] },
+      { id: 4, name: '피카츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 5, name: '이브이', image: '/img1.png', abilities: ['적응력', '도주'] },
+      { id: 6, name: '뮤', image: '/img1.png', abilities: ['싱크로'] }
+    ]
+  },
+  {
+    id: 5,
+    name: 'user5',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 5, hashtag: '진화의 신비' }],
+    pokemons: [
+      { id: 7, name: '뮤츠', image: '/img1.png', abilities: ['프레셔', '정신력'] },
+      { id: 8, name: '라이츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 9, name: '리자몽', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 10, name: '프리저', image: '/img1.png', abilities: ['눈보라', '빙판'] },
+      { id: 1, name: '이상해씨', image: '/img1.png', abilities: ['심록', '엽록소'] }
+    ]
+  },
+  {
+    id: 6,
+    name: 'user6',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 1, hashtag: '갸라도스의 폭풍속에서' }],
+    pokemons: [
+      { id: 2, name: '파이리', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 3, name: '꼬부기', image: '/img1.png', abilities: ['급류', '빗의접시'] },
+      { id: 4, name: '피카츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 5, name: '이브이', image: '/img1.png', abilities: ['적응력', '도주'] }
+    ]
+  },
+  {
+    id: 7,
+    name: 'user7',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 2, hashtag: '내일은 세계최고의 포켓몬트레이너' }],
+    pokemons: [
+      { id: 6, name: '뮤', image: '/img1.png', abilities: ['싱크로'] },
+      { id: 7, name: '뮤츠', image: '/img1.png', abilities: ['프레셔', '정신력'] },
+      { id: 8, name: '라이츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 9, name: '리자몽', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 10, name: '프리저', image: '/img1.png', abilities: ['눈보라', '빙판'] }
+    ]
+  },
+  {
+    id: 8,
+    name: 'user8',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 3, hashtag: '포켓몬 마스터를 향해' }],
+    pokemons: [
+      { id: 1, name: '이상해씨', image: '/img1.png', abilities: ['심록', '엽록소'] },
+      { id: 2, name: '파이리', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 3, name: '꼬부기', image: '/img1.png', abilities: ['급류', '빗의접시'] },
+      { id: 4, name: '피카츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 5, name: '이브이', image: '/img1.png', abilities: ['적응력', '도주'] }
+    ]
+  },
+  {
+    id: 9,
+    name: 'user9',
+    profileImage: '/img1.png',
+    hashtags: [{ id: 4, hashtag: '전설의 포켓몬을 찾아서' }],
+    pokemons: [
+      { id: 6, name: '뮤', image: '/img1.png', abilities: ['싱크로'] },
+      { id: 7, name: '뮤츠', image: '/img1.png', abilities: ['프레셔', '정신력'] },
+      { id: 8, name: '라이츄', image: '/img1.png', abilities: ['정전기', '피뢰침'] },
+      { id: 9, name: '리자몽', image: '/img1.png', abilities: ['맹화', '선파워'] },
+      { id: 10, name: '프리저', image: '/img1.png', abilities: ['눈보라', '빙판'] },
+      { id: 1, name: '이상해씨', image: '/img1.png', abilities: ['심록', '엽록소'] }
+    ]
+  }
+];
+
+export default users;
--- src/app/(providers)/(root)/(mypage)/mypage/[id]/page.tsx
+++ src/app/(providers)/(root)/(mypage)/mypage/[id]/page.tsx
@@ -1,21 +1,36 @@
+'use client';
+
+import { useParams } from 'next/navigation';
 import Image from 'next/image';
 import React from 'react';
-import pokemonData from './dumy';
-import { hashtagData } from './dumy';
+import users from './dumy';
 
 const Page: React.FC = () => {
+  const { id } = useParams();
+
+  if (!id) {
+    return <div>유저를 찾을 수 없습니다.</div>;
+  }
+
+  const userId = parseInt(id as string, 10);
+  const user = users.find((user) => user.id === userId);
+
+  if (!user) {
+    return <div>유저를 찾을 수 없습니다.</div>;
+  }
+
   return (
     <div className="flex min-h-screen justify-center bg-gray-100 py-10">
       <div className="w-[600px] rounded-lg bg-white p-7 shadow-lg">
-        {/* <h1 className="text-center text-3xl font-bold mb-8">여기는 마이페이지다</h1> */}
         <div className="mb-8 mt-5">
           <div className="flex items-center justify-between rounded-md bg-gray-200 p-4">
             <div className="flex items-center">
-              <Image src="/img1.png" alt="userImage" width={100} height={100} className="rounded-full" />
-              <div className="ml-4">
-                {hashtagData.map((hashtag) => (
+              <Image src={user.profileImage} alt="userImage" width={100} height={100} className="rounded-full" />
+              <div className="ml-5 flex flex-col">
+                <div className="text-lg font-bold mb-2">{user.name}</div>
+                {user.hashtags.map((hashtag) => (
                   <div key={hashtag.id}>
-                    <h4 className="font-bold">#{hashtag.hashtag}</h4>
+                    <h4 className="font-ligter">#{hashtag.hashtag}</h4>
                   </div>
                 ))}
               </div>
@@ -25,16 +40,11 @@ const Page: React.FC = () => {
         </div>
         <h2 className="mb-4 text-2xl font-bold">내 포켓몬</h2>
         <div className="mb-5 grid grid-cols-3 gap-8">
-          {pokemonData.map((mypokemon) => (
+          {user.pokemons.map((mypokemon) => (
             <div key={mypokemon.id} className="rounded-lg bg-gray-100 p-4 shadow-sm">
               <div className="flex flex-col items-center">
                 <div className="relative mb-4 h-24 w-24">
-                  <Image
-                    src={mypokemon.image}
-                    alt={mypokemon.name}
-                    layout="fill"
-                    className="rounded-full object-cover"
-                  />
+                  <Image src={mypokemon.image} alt={mypokemon.name} fill className="rounded-full object-cover" />
                 </div>
                 <h3 className="mb-2 font-bold">{mypokemon.name}</h3>
                 <button className="rounded-md bg-gray-400 px-2 py-1">상세정보</button>
