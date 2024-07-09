19e9b5f8a80f2d0e989c2a6136be5f184bd52de5
hwangseunghyun <hwamg123@naver.com>
2024-07-09 15:17:39 +0900

    Feat : 해쉬태그더미데이터 추가 , 해쉬태그 맵핑 적용

--- src/app/(providers)/(root)/(mypage)/mypage/dumy.ts
+++ src/app/(providers)/(root)/(mypage)/mypage/dumy.ts
@@ -1,65 +1,81 @@
+// src/data/pokemon.ts
+
 export interface Pokemon {
-    id: number;
-    name: string;
-    image: string;
-    abilities: string[];
+  id: number;
+  name: string;
+  image: string;
+  abilities: string[];
+}
+
+export interface Hashtags {
+  id: number;
+  hashtag: string;
+}
+
+const pokemonData: Pokemon[] = [
+  {
+    id: 1,
+    name: '이상해씨',
+    image: '/img1.png',
+    abilities: ['심록', '엽록소']
+  },
+  {
+    id: 2,
+    name: '파이리',
+    image: '/img1.png',
+    abilities: ['맹화', '선파워']
+  },
+  {
+    id: 3,
+    name: '꼬부기',
+    image: '/img1.png',
+    abilities: ['급류', '빗의접시']
+  },
+  {
+    id: 4,
+    name: '피카츄',
+    image: '/img1.png',
+    abilities: ['정전기', '피뢰침']
+  },
+  {
+    id: 5,
+    name: '이브이',
+    image: '/img1.png',
+    abilities: ['적응력', '도주']
+  },
+  {
+    id: 6,
+    name: '뮤',
+    image: '/img1.png',
+    abilities: ['싱크로']
+  },
+  {
+    id: 7,
+    name: '뮤츠',
+    image: '/img1.png',
+    abilities: ['프레셔', '정신력']
+  },
+  {
+    id: 8,
+    name: '라이츄',
+    image: '/img1.png',
+    abilities: ['정전기', '피뢰침']
+  },
+  {
+    id: 9,
+    name: '리자몽',
+    image: '/img1.png',
+    abilities: ['맹화', '선파워']
   }
-  
-  const pokemonData: Pokemon[] = [
-    {
-      id: 1,
-      name: "Bulbasaur",
-      image: "/img1.png",
-      abilities: ["Overgrow", "Chlorophyll"],
-    },
-    {
-      id: 2,
-      name: "Charmander",
-      image: "/img1.png",
-      abilities: ["Blaze", "Solar Power"],
-    },
-    {
-      id: 3,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 4,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 5,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 6,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 7,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 8,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-    {
-      id: 9,
-      name: "Squirtle",
-      image: "/img1.png",
-      abilities: ["Torrent", "Rain Dish"],
-    },
-  ];
-  
-  export default pokemonData;
+];
+
+export default pokemonData;
+
+const hashtagData: Hashtags[] = [
+  { id: 1, hashtag: '갸라도스의 폭풍속에서' },
+  {
+    id: 2,
+    hashtag: '내일은 세계최고의 포켓몬트레이너'
+  }
+];
+export { hashtagData };
--- src/app/(providers)/(root)/(mypage)/mypage/page.tsx
+++ src/app/(providers)/(root)/(mypage)/mypage/page.tsx
@@ -1,30 +1,34 @@
 import Image from 'next/image';
 import React from 'react';
 import pokemonData from './dumy';
+import { hashtagData } from './dumy';
 
 const Page: React.FC = () => {
   return (
-    <div className="flex justify-center min-h-screen bg-gray-100 py-10">
-      <div className="bg-white w-[600px] p-7 rounded-lg shadow-lg">
+    <div className="flex min-h-screen justify-center bg-gray-100 py-10">
+      <div className="w-[600px] rounded-lg bg-white p-7 shadow-lg">
         {/* <h1 className="text-center text-3xl font-bold mb-8">여기는 마이페이지다</h1> */}
         <div className="mb-8 mt-5">
-          <div className="bg-gray-200 p-4 rounded-md flex items-center justify-between">
+          <div className="flex items-center justify-between rounded-md bg-gray-200 p-4">
             <div className="flex items-center">
               <Image src="/img1.png" alt="userImage" width={100} height={100} className="rounded-full" />
               <div className="ml-4">
-                <h4 className="font-bold">#갸라도스의 폭풍 속에서</h4>
-                {/* 여기에 뭐를 어떻게 넣을까 */}
+                {hashtagData.map((hashtag) => (
+                  <div key={hashtag.id}>
+                    <h4 className="font-bold">#{hashtag.hashtag}</h4>
+                  </div>
+                ))}
               </div>
             </div>
-            <button className="bg-gray-400 px-2 py-1 rounded-md">수정</button>
+            <button className="rounded-md bg-gray-400 px-2 py-1">수정</button>
           </div>
         </div>
-        <h2 className="text-2xl font-bold mb-4">내 포켓몬</h2>
-        <div className="grid grid-cols-3 gap-8">
+        <h2 className="mb-4 text-2xl font-bold">내 포켓몬</h2>
+        <div className="mb-5 grid grid-cols-3 gap-8">
           {pokemonData.map((mypokemon) => (
             <div key={mypokemon.id} className="rounded-lg bg-gray-100 p-4 shadow-sm">
               <div className="flex flex-col items-center">
-                <div className="relative h-24 w-24 mb-4">
+                <div className="relative mb-4 h-24 w-24">
                   <Image
                     src={mypokemon.image}
                     alt={mypokemon.name}
@@ -32,8 +36,8 @@ const Page: React.FC = () => {
                     className="rounded-full object-cover"
                   />
                 </div>
-                <h3 className="font-bold mb-2">{mypokemon.name}</h3>
-                <button className="bg-gray-400 px-2 py-1 rounded-md">상세정보</button>
+                <h3 className="mb-2 font-bold">{mypokemon.name}</h3>
+                <button className="rounded-md bg-gray-400 px-2 py-1">상세정보</button>
               </div>
             </div>
           ))}
