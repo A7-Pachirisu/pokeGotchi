77f9551a46ef09b3132f3bc2dc802f72615234ae
hayanLee <hayanlee.dev@gmail.com>
2024-07-11 03:07:24 +0900

    Feat : authContext 적용
    - 로그인, 회원가입, 로그아웃, 유저정보

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,17 +1,17 @@
 import api from '@/lib/axios';
 import { logInForm } from '@/types/formType';
 
-export const signUp = async (signUpData: logInForm) => {
-  const response = await api.post('/api/auth/sign-up', signUpData);
+export const signUpService = async ({ email, password }: logInForm) => {
+  const response = await api.post('/api/auth/sign-up', { email, password });
   return response.data;
 };
 
-export const logIn = async (logInData: logInForm) => {
+export const logInService = async (logInData: logInForm) => {
   const response = await api.post('/api/auth/log-in', logInData);
   return response.data;
 };
 
-export const logOut = async () => {
+export const logOutService = async () => {
   const response = await api.delete('/api/auth/log-out');
   return response.data;
 };
