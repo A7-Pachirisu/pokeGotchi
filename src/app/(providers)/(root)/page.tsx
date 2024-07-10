2c67f4064cd92080ed908b80fa1d3a3a743f2e17
hwijinkim22 <bout9396@naver.com>
2024-07-10 14:31:22 +0900

    Fix: useEffect 중복 제거

--- src/app/(providers)/(root)/page.tsx
+++ src/app/(providers)/(root)/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import api from '@/lib/axios';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import backgroundImage from '@/assets/background.png';
 
 interface Pokemon {
