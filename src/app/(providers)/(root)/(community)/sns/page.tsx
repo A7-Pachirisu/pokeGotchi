787046b02646851c3402dfc7154f52eab8d0766a
FE_sunmin <fe.sunmin@gmail.com>
2024-07-10 21:35:31 +0900

    Chore: 업로드 페이지 이동 Link 설정

--- src/app/(providers)/(root)/(community)/sns/page.tsx
+++ src/app/(providers)/(root)/(community)/sns/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import RankItem from './_components/RankItem';
 import PostItem from './_components/PostItem';
+import Link from 'next/link';
+import { FaPencil } from 'react-icons/fa6';
 
 function page() {
   return (
@@ -10,7 +12,12 @@ function page() {
         <RankItem />
       </header>
       <main className="mt-16">
-        <h1 className="ml-10 text-lg font-semibold">자랑하기</h1>
+        <div className="mr-[70px] mt-2 flex justify-between text-xl">
+          <h1 className="ml-10 text-lg font-semibold">자랑하기</h1>
+          <Link href="/upload">
+            <FaPencil />
+          </Link>
+        </div>
         <PostItem />
       </main>
     </div>
--- src/app/(providers)/(root)/(community)/upload/_components/FormCard.tsx
+++ src/app/(providers)/(root)/(community)/upload/_components/FormCard.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import profile1 from '../../../../../../assets/random profile1.png';
 import Image from 'next/image';
 import { FaRegImage } from 'react-icons/fa6';
 
 const FormCard = () => {
   return (
-    <div className="mt-5 flex w-full flex-col items-center">
+    <div className="mt-10 flex w-full flex-col items-center">
       <div className="mb-5 flex w-[450px] items-center">
         <Image src={profile1} alt="profileImg" className="h-[50px] w-[50px] rounded-full border" />
         <p className="ml-5">ooo 님</p>
@@ -13,7 +13,8 @@ const FormCard = () => {
       <main className="flex h-[400px] w-[450px] flex-col items-center justify-between rounded-xl border">
         <textarea placeholder="내용을 입력하세요..." className="mt-5 h-[200px] w-[420px] resize-none pl-[10px] pt-5" />
         <div className="mb-5 ml-5 w-[420px] text-2xl">
-          <FaRegImage />
+          <input type="file" />
+          <FaRegImage style={{ cursor: 'pointer', fontSize: '24px' }} />
         </div>
       </main>
     </div>
