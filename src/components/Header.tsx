4cb710d966449e2b5150b1ec470e344ecd0c5d31
hayanLee <hayanlee.dev@gmail.com>
2024-07-11 02:31:17 +0900

    Design : Header, MenuBar UI 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
+'use client';
 import Image from 'next/image';
-import img from '../assets/default ball.png';
 import Link from 'next/link';
+import img from '../assets/default ball.png';
 
 const Header = () => {
+  const handleClick = () => {
+    console.log('1');
+  };
+
   return (
-    <header className="flex h-[70px] w-full items-center justify-center bg-bar-color">
+    <header className="relative flex h-[70px] w-full items-center justify-center bg-bar-color px-4">
       <Link href="/">
         <Image src={img} alt="" width={40} height={40} />
       </Link>
-      <Link className="pl-10 text-white" href="/log-in">
-        Login
-      </Link>
+      <button
+        className="absolute right-4 rounded bg-custom-yellow p-1.5 px-2 text-lg uppercase text-black hover:brightness-95 active:brightness-90"
+        onClick={handleClick}
+      >
+        Logout
+      </button>
     </header>
   );
 };
