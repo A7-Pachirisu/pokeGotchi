6c90fc5b1b401e31757201d232599514db1b03c8
hayanLee <hayanlee.dev@gmail.com>
2024-07-11 15:52:57 +0900

    Design : 게임 로비 Link 공통 컴포넌트로 변경
    - LobbyButton 생성

---
+++ src/app/(providers)/(root)/game/_components/LobbyButton.tsx
@@ -0,0 +1,18 @@
+import PokeBall from '@/assets/default ball.png';
+import Button, { ButtonProps } from '@/components/Button';
+import Image from 'next/image';
+
+type LobbyButton = {
+  title: string;
+} & ButtonProps;
+
+function LobbyButton({ title, ...props }: LobbyButton) {
+  return (
+    <Button {...props}>
+      <Image src={PokeBall} alt="" width={80} height={80} className="mr-10" />
+      <span className="grow">{title}</span>
+    </Button>
+  );
+}
+
+export default LobbyButton;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,33 +3,37 @@ import Link from 'next/link';
 import { ComponentProps, PropsWithChildren } from 'react';
 import { twMerge } from 'tailwind-merge';
 
-const buttonVariant = cva('font-semibold text-white rounded text-center hover:brightness-105 active:brightness-110', {
-  variants: {
-    size: {
-      sm: '', // 상단바
-      md: '', // 상점, sns
-      lg: 'px-1.5 py-2', // 로그인
-      xl: '' // 로비,
+const buttonVariant = cva(
+  'flex font-semibold justify-center  text-white rounded items-center  hover:brightness-105 active:brightness-110',
+  {
+    variants: {
+      size: {
+        sm: '', // 상단바
+        md: '', // 상점, sns
+        lg: 'px-1.5 py-2', // 로그인
+        xl: 'px-6 py-3.5 w-full text-4xl' // 로비,
+      },
+      intent: {
+        yellow: 'bg-custom-yellow text-black',
+        red: 'bg-custom-red',
+        blue: 'bg-custom-blue',
+        grey: 'bg-custom-grey',
+        black: 'bg-custom-black',
+        green: 'bg-custom-green',
+        white: 'bg-custom-white'
+      }
     },
-    intent: {
-      yellow: 'bg-custom-yellow text-black',
-      red: 'bg-custom-red',
-      blue: 'bg-custom-blue',
-      grey: 'bg-custom-grey',
-      black: 'bg-custom-black',
-      green: 'bg-custom-green',
-      white: 'bg-custom-white'
+    compoundVariants: [],
+    defaultVariants: {
+      size: 'md',
+      intent: 'blue'
     }
-  },
-  compoundVariants: [],
-  defaultVariants: {
-    size: 'md',
-    intent: 'blue'
   }
-});
+);
 
 type ButtonrVariant = VariantProps<typeof buttonVariant>;
-type ButtonProps = ButtonrVariant & (ComponentProps<'button'> | ({ href: string } & ComponentProps<typeof Link>));
+export type ButtonProps = ButtonrVariant &
+  (ComponentProps<'button'> | ({ href: string } & ComponentProps<typeof Link>));
 
 function Button({ intent, size, children, ...props }: PropsWithChildren<ButtonProps>) {
   if ('href' in props) {
