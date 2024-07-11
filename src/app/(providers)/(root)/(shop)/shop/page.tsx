c2eab96be96ad7939e94b8d8850b90a5d569325a
hayanLee <hayanlee.dev@gmail.com>
2024-07-11 17:58:50 +0900

    Refactor : PokeCard 로 컴포넌트 분리

--- src/app/(providers)/(root)/(shop)/shop/page.tsx
+++ src/app/(providers)/(root)/(shop)/shop/page.tsx
@@ -2,9 +2,8 @@
 import Button from '@/components/Button';
 import { Pokemon } from '@/types/pokemonType';
 import { useQuery } from '@tanstack/react-query';
-import Image from 'next/image';
-import Link from 'next/link';
 import { BiCoinStack } from 'react-icons/bi';
+import PokeCard from './_components/PokeCard';
 
 const getPokemons = async () => {
   const res = await fetch('http://localhost:3000/api/shop');
@@ -36,6 +35,7 @@ const ShopPage = () => {
   return (
     <>
       <div className="align-center mx-auto my-auto mt-7 flex justify-center text-center text-3xl">포켓몬 상점</div>
+
       <div className="m-5 flex justify-between">
         <Button intent="white" size="md">
           <span>Lv.1</span>
@@ -45,38 +45,12 @@ const ShopPage = () => {
           <span>50</span>
         </Button>
       </div>
+
       <div className="mx-auto mt-10 grid grid-cols-3 gap-3 text-center">
         {pokemons?.map((pokemon) => {
-          return (
-            <div key={pokemon.id} className="my-1 rounded-md border border-gray-300 p-3 shadow-lg">
-              <Link href={`/shopDetail/${pokemon.id}`}>
-                <div className="align-center flex justify-end text-xl">
-                  <BiCoinStack className="my-auto text-yellow-400" />
-                  50
-                </div>
-                <div className="align-center mx-auto flex h-[100px] w-[100px] justify-center">
-                  <Image
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${pokemon.id}.gif`}
-                    alt={'pokemon_img'}
-                    unoptimized
-                    width={100}
-                    height={100}
-                  />
-                </div>
-
-                <div>
-                  <div className="my-2 text-xl">{pokemon.korean_name}</div>
-                </div>
-
-                <button className="border-custom-green bg-custom-green rounded-md border px-3 text-lg text-white shadow-sm shadow-black hover:brightness-95">
-                  Buy
-                </button>
-              </Link>
-            </div>
-          );
+          return <PokeCard key={pokemon.id} pokemon={pokemon} />;
         })}
       </div>
-      <div />
     </>
   );
 };
