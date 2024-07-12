d43b03eca5c1a529e7825d6fc4df5e3e91f96540
hyorimhan <mynameishan8@gmail.com>
2024-07-13 00:13:12 +0900

    Fix: 타입 에러 수정

---
+++ src/services/supabaseService.ts
@@ -0,0 +1,38 @@
+import { createClient } from '@/supabase/client';
+
+const supabase = createClient();
+
+export const getUserInfo = async () => {
+  // 유저 정보
+  const {
+    data: { user },
+    error
+  } = await supabase.auth.getUser();
+
+  if (error) console.error('에러가 발생했습니다:', error.message);
+  return user;
+};
+
+export const coinAndNicknameInfo = async (userId: string) => {
+  //코인, 닉네임
+  const { data: coinNickname, error } = await supabase
+    .from('users')
+    .select('coins, nickname')
+    .eq('id', userId)
+    .single();
+  if (error) console.error('에러가 발생했습니다:', error.message);
+  return coinNickname;
+};
+
+export const ownedPokemonList = async (userId: string) => {
+  //보유 포켓몬
+  const { data: own, error } = await supabase.from('user_pokemons').select('pokemonNumber').eq('userId', userId);
+  if (error) console.error('에러가 발생했습니다:', error.message);
+  return own ? own.map((pokemon) => pokemon.pokemonNumber!).filter((pokemonNumber) => pokemonNumber !== null) : [];
+};
+
+export const updateCoin = async (userId: string, coins: number) => {
+  //코인 업데이트
+  const { error } = await supabase.from('users').update({ coins }).eq('id', userId);
+  if (error) console.error('에러가 발생했습니다:', error.message);
+};
--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -1,223 +1,207 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export type Database = {
   public: {
     Tables: {
       purchase_history: {
         Row: {
-          createdAt: string
-          id: string
-          pokemonId: string | null
-          pokemonNumber: number | null
-          userId: string | null
-        }
+          createdAt: string;
+          id: string;
+          pokemonId: string | null;
+          pokemonNumber: number | null;
+          userId: string | null;
+        };
         Insert: {
-          createdAt?: string
-          id?: string
-          pokemonId?: string | null
-          pokemonNumber?: number | null
-          userId?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          pokemonId?: string | null;
+          pokemonNumber?: number | null;
+          userId?: string | null;
+        };
         Update: {
-          createdAt?: string
-          id?: string
-          pokemonId?: string | null
-          pokemonNumber?: number | null
-          userId?: string | null
-        }
+          createdAt?: string;
+          id?: string;
+          pokemonId?: string | null;
+          pokemonNumber?: number | null;
+          userId?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "purchase_history_pokemonId_fkey"
-            columns: ["pokemonId"]
-            isOneToOne: false
-            referencedRelation: "user_pokemons"
-            referencedColumns: ["pokemonId"]
+            foreignKeyName: 'purchase_history_pokemonId_fkey';
+            columns: ['pokemonId'];
+            isOneToOne: false;
+            referencedRelation: 'user_pokemons';
+            referencedColumns: ['pokemonId'];
           },
           {
-            foreignKeyName: "purchase_history_userId_fkey"
-            columns: ["userId"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'purchase_history_userId_fkey';
+            columns: ['userId'];
+            isOneToOne: false;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       user_pokemons: {
         Row: {
-          createdAt: string
-          id: string
-          pokemonId: string | null
-          pokemonNumber: number | null
-          status: Json | null
-          userId: string
-        }
+          createdAt: string;
+          id: string;
+          pokemonId: string | null;
+          pokemonNumber: number | null;
+          status: Json | null;
+          userId: string;
+        };
         Insert: {
-          createdAt?: string
-          id?: string
-          pokemonId?: string | null
-          pokemonNumber?: number | null
-          status?: Json | null
-          userId?: string
-        }
+          createdAt?: string;
+          id?: string;
+          pokemonId?: string | null;
+          pokemonNumber?: number | null;
+          status?: Json | null;
+          userId?: string;
+        };
         Update: {
-          createdAt?: string
-          id?: string
-          pokemonId?: string | null
-          pokemonNumber?: number | null
-          status?: Json | null
-          userId?: string
-        }
+          createdAt?: string;
+          id?: string;
+          pokemonId?: string | null;
+          pokemonNumber?: number | null;
+          status?: Json | null;
+          userId?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "user_pokemons_userId_fkey"
-            columns: ["userId"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'user_pokemons_userId_fkey';
+            columns: ['userId'];
+            isOneToOne: false;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       users: {
         Row: {
-          coins: number | null
-          createdAt: string
-          email: string
-          gameScore_ball: number | null
-          gameScore_fruit: number | null
-          gameScore_quiz: number | null
-          id: string
-          nickName: string | null
-        }
+          coins: number | null;
+          createdAt: string;
+          email: string;
+          gameScore_ball: number | null;
+          gameScore_fruit: number | null;
+          gameScore_quiz: number | null;
+          id: string;
+          nickname: string | null;
+        };
         Insert: {
-          coins?: number | null
-          createdAt?: string
-          email: string
-          gameScore_ball?: number | null
-          gameScore_fruit?: number | null
-          gameScore_quiz?: number | null
-          id?: string
-          nickName?: string | null
-        }
+          coins?: number | null;
+          createdAt?: string;
+          email: string;
+          gameScore_ball?: number | null;
+          gameScore_fruit?: number | null;
+          gameScore_quiz?: number | null;
+          id?: string;
+          nickname?: string | null;
+        };
         Update: {
-          coins?: number | null
-          createdAt?: string
-          email?: string
-          gameScore_ball?: number | null
-          gameScore_fruit?: number | null
-          gameScore_quiz?: number | null
-          id?: string
-          nickName?: string | null
-        }
+          coins?: number | null;
+          createdAt?: string;
+          email?: string;
+          gameScore_ball?: number | null;
+          gameScore_fruit?: number | null;
+          gameScore_quiz?: number | null;
+          id?: string;
+          nickname?: string | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "users_id_fkey"
-            columns: ["id"]
-            isOneToOne: true
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-    }
+            foreignKeyName: 'users_id_fkey';
+            columns: ['id'];
+            isOneToOne: true;
+            referencedRelation: 'users';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
 
 export type Tables<
-  PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views']) | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] & PublicSchema['Views'])
+    ? (PublicSchema['Tables'] & PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema['Tables'] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+    ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
+  PublicEnumNameOrOptions extends keyof PublicSchema['Enums'] | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+    ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+    : never;
