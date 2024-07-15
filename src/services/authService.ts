6b9e5f9635e78f6c3516536e864fe635722ec77c
hayanLee <hayanlee.dev@gmail.com>
2024-07-15 11:05:31 +0900

    Fix : axios 경로 수정

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,29 +1,29 @@
-import api from '@/lib/axios';
 import { logInForm } from '@/types/formType';
+import axios from 'axios';
 
 export const signUpService = async ({ email, password }: logInForm) => {
-  const response = await api.post('/api/auth/sign-up', { email, password });
+  const response = await axios.post('/api/auth/sign-up', { email, password });
   return response.data;
 };
 
 export const logInService = async (logInData: logInForm) => {
-  const response = await api.post('/api/auth/log-in', logInData);
+  const response = await axios.post('/api/auth/log-in', logInData);
   return response.data;
 };
 
 export const logInWithKaKaoService = async () => {
-  const response = await api.get('/api/auth/provider');
+  const response = await axios.get('/api/auth/provider');
   return response.data;
 };
 
 export const logOutService = async () => {
-  const response = await api.delete('/api/auth/log-out');
+  const response = await axios.delete('/api/auth/log-out');
   return response.data;
 };
 
 export const getUserService = async () => {
   try {
-    const response = await api.get('/api/auth/me');
+    const response = await axios.get('/api/auth/me');
     return response.data;
   } catch (err) {
     if (err instanceof Error) throw new Error(err.message);
